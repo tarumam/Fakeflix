d3cc3946c583ae177bfa40d920d826edf01346aa
Tarumã <tarumamiranda@gmail.com>
2020-08-06 20:33:22 -0300

    checkin para server heroku

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CadastroVideo from './pages/Cadastro/Video';
 import CadastroCategoria from './pages/Cadastro/Categoria';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/" component={Home} exact/>
-      <Route path="/cadastro/categoria" component={CadastroCategoria} exact/>
-      <Route path="/cadastro/video" component={CadastroVideo} exact/>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} exact />
       <Route component={() => <div>404</div>} />
     </Switch>
   </BrowserRouter>,
-  document.getElementById('root')
-)
+  document.getElementById('root'),
+);
